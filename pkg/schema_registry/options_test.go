7ec2f9560e7494bcf7bf3b83cfd5dfba3daa1ae8
xBlaz3kx <blaz.dular@gmail.com>
2025-06-07 23:00:37 +0200

    Added options tests

--- pkg/schema_registry/options_test.go
+++ pkg/schema_registry/options_test.go
@@ -10,7 +10,39 @@ type optionsTestSuite struct {
 	suite.Suite
 }
 
-func (s *optionsTestSuite) TestNewoptions() {
+func (s *optionsTestSuite) TestOptions() {
+	tests := []struct {
+		name     string
+		opts     []Option
+		expected Options
+	}{
+		{
+			name: "default options",
+			opts: []Option{},
+			expected: Options{
+				overwrite: false,
+			},
+		},
+		{
+			name: "WithOverwrite",
+			opts: []Option{
+				WithOverwrite(true),
+			},
+			expected: Options{
+				overwrite: true,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		s.Run(tt.name, func() {
+			options := &Options{}
+			for _, opt := range tt.opts {
+				opt(options)
+			}
+			s.Equal(tt.expected, *options)
+		})
+	}
 
 }
 
