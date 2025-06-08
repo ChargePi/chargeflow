65d5c55ef6b179da74a4a483de158a0b33ad9dfb
xBlaz3kx <blaz.dular@gmail.com>
2025-06-08 10:14:37 +0200

    Add results tests

--- pkg/validator/result_test.go
+++ pkg/validator/result_test.go
@@ -1,1 +1,37 @@
 package validator
+
+import (
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type resultTestSuite struct {
+	suite.Suite
+}
+
+func (s *resultTestSuite) TestAddError() {
+	result := NewValidationResult()
+	s.True(result.isValid)
+
+	result.AddError("test error")
+	s.False(result.IsValid())
+	s.Contains(result.Errors(), "test error")
+}
+
+func (s *resultTestSuite) TestErrors() {
+	result := NewValidationResult()
+	s.True(result.isValid)
+
+	result.AddError("first error")
+	result.AddError("second error")
+
+	errors := result.Errors()
+	s.Len(errors, 2)
+	s.Contains(errors, "first error")
+	s.Contains(errors, "second error")
+	s.NotContains(errors, "third error")
+}
+
+func TestRegistry(t *testing.T) {
+	suite.Run(t, new(resultTestSuite))
+}
