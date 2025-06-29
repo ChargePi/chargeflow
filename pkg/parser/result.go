bf4234f5ee34179c35c106c4896b2b1c6c0fce76
xBlaz3kx <blaz.dular@gmail.com>
2025-06-29 10:08:25 +0200

    fix: updated result to contain the ocpp message (needed for validation)

--- pkg/parser/result.go
+++ pkg/parser/result.go
@@ -3,11 +3,12 @@ package parser
 import "github.com/ChargePi/chargeflow/pkg/ocpp"
 
 type Result struct {
+	message ocpp.Message
 	isValid bool
 	errors  []string
 }
 
-// NewResult creates a new ValidationResult with the given validity and errors.
+// NewResult creates a new Result with the given validity and errors.
 func NewResult() *Result {
 	return &Result{
 		isValid: true,
@@ -31,13 +32,53 @@ func (v *Result) Errors() []string {
 	return v.errors
 }
 
-type RequestResponsePairResult struct {
+func (v *Result) Message() ocpp.Message {
+	return v.message
+}
+
+func (v *Result) SetMessage(message ocpp.Message) {
+	v.message = message
+}
+
+type RequestResponseResult struct {
 	// Request is the parsed OCPP request message.
-	Request ocpp.Message
+	Request Result
 
 	// Response can be either ocpp.CallResult or ocpp.CallError (or nil if no request was provided).
-	Response ocpp.Message
+	Response Result
+}
 
-	// Result contains the parsing validation result.
-	Result Result
+func NewRequestResponseResult() *RequestResponseResult {
+	return &RequestResponseResult{
+		Request:  *NewResult(),
+		Response: *NewResult(),
+	}
+}
+
+func (r *RequestResponseResult) AddRequestError(err string) {
+	r.Request.AddError(err)
+}
+
+func (r *RequestResponseResult) AddResponseError(err string) {
+	r.Response.AddError(err)
+}
+
+func (r *RequestResponseResult) AddRequest(request ocpp.Message) {
+	r.Request.SetMessage(request)
+}
+
+func (r *RequestResponseResult) AddResponse(response ocpp.Message) {
+	r.Response.SetMessage(response)
+}
+
+func (r *RequestResponseResult) IsValid() bool {
+	return r.Request.IsValid() && r.Response.IsValid()
+}
+
+func (r *RequestResponseResult) GetRequest() (ocpp.Message, bool) {
+	return r.Request.message, r.Request.message != nil
+}
+
+func (r *RequestResponseResult) GetResponse() (ocpp.Message, bool) {
+	return r.Response.message, r.Response.message != nil
 }
